09b75ce6247d6c12a7385e899b001be82bbe26e6
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-05 15:21:02 -0500

    Updated from Brython Server: 11/5/2015 3:21:02 PM

--- Word.py
+++ Word.py
@@ -2,8 +2,4 @@
 Name: Suhan
 Credit: me
 """
-problem=input("What is your is word problem for math?(Grammar, spelling, and punctuation counts!)\n\n")
-print("")
-print("Lets break it down")
-print("")
-print("
+from ggame import TextAsset
