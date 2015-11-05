44305e3d790906470e058a6f8208c63e115f0b7f
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-05 14:53:21 -0500

    Updated from Brython Server: 11/5/2015 2:53:21 PM

--- Word.py
+++ Word.py
@@ -1,1 +1,9 @@
+"""
+Name: Suhan
+Credit: me
+"""
 problem=input("What is your is word problem for math?(Grammar, spelling, and punctuation counts!)\n\n")
+print("")
+print("Lets break it down")
+print("")
+print("
