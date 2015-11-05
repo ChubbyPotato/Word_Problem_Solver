1708f262c6b9a32251bc3180cad0251a18fd2a91
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-05 18:04:41 -0500

    Updated from Brython Server: 11/5/2015 6:04:41 PM

--- Word.py
+++ Word.py
@@ -2,5 +2,8 @@ import re
 
 problem=input("What is your mathematical word problem? (Spell, grammar, and punctuation count!)\n\n")
 while problem=="":
-    problem=input("Please enter your mathematical word problem? (Spell, grammar, and punctuation count!)\n\n")
+    problem=input("Please enter your mathematical word problem. (Spell, grammar, and punctuation count!)\n\n")
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
+fake=len(sentences)
+realnum=fake-1
+sent_list=list(range(1,realnum))
