93b70c842b29f0f3ce68ef0912103af561ec951b
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-09 14:43:35 -0500

    Updated from Brython Server: 11/9/2015 2:43:35 PM

--- Word.py
+++ Word.py
@@ -9,14 +9,11 @@ while problem=="":
 
 quest=(dict(map(lambda letter:(letter,len(problem)-len(problem.replace(letter,''))),problem)))
 converted_quest = list(quest.items())
-converted_quest.sort(key=lambda f: (-f[1], f[0]))
-for x in converted_quest:
-    if x[0]!=0 and x[0]!=(" ") and x[0]!=("'")
-        print(x[0]*x[1])
 
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
 num=len(sentences)
 sent_list=list(range(1,num))
 for x in sent_list:
     sents=sentences[x-1]
+    sents.split()
     print(sents)
