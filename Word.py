9a8bde0e37b9a0ad613cdc28333397f09fcedcd7
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-09 14:44:58 -0500

    Updated from Brython Server: 11/9/2015 2:44:58 PM

--- Word.py
+++ Word.py
@@ -7,6 +7,9 @@ problem=input("What is your mathematical word problem? (Spelling, grammar, and p
 while problem=="":
     problem=input("Please enter your mathematical word problem. (Spelling, grammar, and punctuation count! Also, please have questions at the end of the word problem!)\n\n")
 
+a="a"
+a=1
+
 quest=(dict(map(lambda letter:(letter,len(problem)-len(problem.replace(letter,''))),problem)))
 converted_quest = list(quest.items())
 
@@ -15,5 +18,5 @@ num=len(sentences)
 sent_list=list(range(1,num))
 for x in sent_list:
     sents=sentences[x-1]
-    sents.split()
+    sents.split(word)
     print(sents)
