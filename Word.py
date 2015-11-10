f1a154d51fa7b29226e2e980b759904a190a5b63
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-10 14:41:19 -0500

    Updated from Brython Server: 11/10/2015 2:41:19 PM

--- Word.py
+++ Word.py
@@ -4,37 +4,36 @@
 import re
 #import enchant
 
-x=""
-while x != "stop":
-    problem=input("What is your mathematical word problem? (Spelling, grammar, and punctuation count! Also, please have the questions at the end of the word problem!)\n\n")
-    while problem=="":
-        problem=input("Please enter your mathematical word problem. (Spelling, grammar, and punctuation count! Also, please have questions at the end of the word problem!)\n\n")
-    
-    problem.replace(".", "&")   #finding the decimals
-    
-    quest=(dict(map(lambda letter:(letter,len(problem)-len(problem.replace(letter,''))),problem)))
-    converted_quest = list(quest.items())
-    
-    sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
-    
-    num=len(sentences)
-    real_num=num-1
-    el_range=range(0,real_num)
-    
-    if real_num > 6:
-        print("
-    
-    for x in el_range:
+problem=input("What is your mathematical word problem? (Spelling, grammar, and punctuation count! Also, please have the questions at the end of the word problem!)\n\n")
+
+if problem=="":
+    problem=input("Please enter your mathematical word problem. (Spelling, grammar, and punctuation count! Also, please have questions at the end of the word problem!)\n\n")
+
+problem.replace(".", "&")   #finding the decimals
+
+quest=(dict(map(lambda letter:(letter,len(problem)-len(problem.replace(letter,''))),problem)))
+converted_quest = list(quest.items())
+
+sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
+
+num=len(sentences)
+real_num=num-1
+el_range=range(0,real_num)
+        
+if real_num > 6:
+    new_problem=("Word problem too long!")
+
+for x in el_range:
         words=sentences[x].split()
-    
-    jeb=len(words)
+
+jeb=len(words)
     print(words)
+
+for x in jeb:
+    ind_words=words[x]
+    print(and_words)
     
-    for x in jeb:
-        ind_words=words[x]
-        print(and_words)
-    
-    problem.replace("&", ".")    #reconverting decimals
-    
-    #Spell checker
-    #enchant.Dict("en_US")
+problem.replace("&", ".")    #reconverting decimals
+
+#Spell checker
+ #enchant.Dict("en_US")
