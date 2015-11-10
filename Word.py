62e2d00681d4b729502174cb79852ddbc0f7a04c
ChubbyPotato <suhangui@hanovernorwichschools.org>
2015-11-10 14:15:46 -0500

    Updated from Brython Server: 11/10/2015 2:15:45 PM

--- Word.py
+++ Word.py
@@ -2,13 +2,13 @@
 # Credit-Stack Overflow, Alexandru Munteanu
 
 import re
-from enchant.checker import SpellChecker
+#import enchant
 
 problem=input("What is your mathematical word problem? (Spelling, grammar, and punctuation count! Also, please have the questions at the end of the word problem!)\n\n")
 while problem=="":
     problem=input("Please enter your mathematical word problem. (Spelling, grammar, and punctuation count! Also, please have questions at the end of the word problem!)\n\n")
 
-problem.replace(".", " & ")   #finding the decimals
+problem.replace(".", "&")   #finding the decimals
 
 quest=(dict(map(lambda letter:(letter,len(problem)-len(problem.replace(letter,''))),problem)))
 converted_quest = list(quest.items())
@@ -16,13 +16,14 @@ converted_quest = list(quest.items())
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
 
 num=len(sentences)
+real_num=num-1
+el_range=range(0,real_num)
 
-for x in sentences:
-    for y in num:
-        words=sentences[y].split()
-        print(words)
+for x in el_range:
+    words=sentences[x].split()
+    print(words)
 
-problem.replace(" & ", ".")    #reconverting decimals
+problem.replace("&", ".")    #reconverting decimals
 
 #Spell checker
 enchant.Dict("en_US")
