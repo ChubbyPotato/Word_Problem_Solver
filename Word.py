6ad457a18a4abf8adeb734294a8fb04dd530e246
Suhan Gui <suhangui@hanovernorwichschools.org>
2015-11-16 10:59:54 -0500

    Updated from Brython Server: 11/16/2015 10:59:53 AM

--- Word.py
+++ Word.py
@@ -2,13 +2,12 @@
 # Credit-Stack Overflow, Alexandru Munteanu
 
 import re
-from decimals import Decimals
 #import enchant
 
 problem=input("What is your mathematical word problem? (Spelling, grammar, and punctuation count!!)\n\nYour problem: ")
 
 deci=re.findall("\d+.\d+", problem)
-float((deci))
+print(deci)
 
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
 
@@ -20,24 +19,26 @@ while num > 11:
     print("\nWord problem too long!")
     break
 
-if num=<1:
+if num==0:
+    print(
+if num<=1:
     words1=sentences[0].split()
-if num=<2:
+if num<=2:
     words2=sentences[1].split()
-if num=<3:
+if num<=3:
     print(words2)
     words3=sentences[2].split()
-if num=<4:
+if num<=4:
     words4=sentences[3].split()
-if num=<5:
+if num<=5:
     words5=sentences[4].split()
-if num=<6:
+if num<=6:
     words6=sentences[5].split()
-if num=<7:
+if num<=7:
     words7=sentences[6].split()
-if num=<8:
+if num<=8:
     words8=sentences[7].split()
-if num=<9:
+if num<=9:
     words9=sentences[8].split()
 if num==10:
     words10=sentences[10].split()
