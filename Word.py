3d31b8187ceccfb72552c2ffcf4226b34a5b84c6
Suhan Gui <suhangui@hanovernorwichschools.org>
2015-11-16 09:50:11 -0500

    Updated from Brython Server: 11/16/2015 9:50:11 AM

--- Word.py
+++ Word.py
@@ -2,15 +2,13 @@
 # Credit-Stack Overflow, Alexandru Munteanu
 
 import re
+from decimals import Decimals
 #import enchant
 
 problem=input("What is your mathematical word problem? (Spelling, grammar, and punctuation count!!)\n\nYour problem: ")
 
-numbers=['1','2','3','4','5','6','7','8','9','0']
-problem2=problem.split()
-problem.replace("{0}.{0}", "&".format(numbers))
-
-print(problem)
+deci=re.findall("\d+.\d+", problem)
+float((deci))
 
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem)
 
@@ -18,54 +16,28 @@ um=len(sentences)
 num=um-1
 el_range=range(0,num)
 
-while num > 9:
+while num > 11:
     print("\nWord problem too long!")
     break
 
-if num==1:
+if num=<1:
     words1=sentences[0].split()
-elif num==2:
-    words1=sentences[0].split()
+if num=<2:
     words2=sentences[1].split()
-elif num==3:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
+if num=<3:
     print(words2)
     words3=sentences[2].split()
-elif num==4:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
-    words3=sentences[2].split()
+if num=<4:
     words4=sentences[3].split()
-elif num==5:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
-    words3=sentences[2].split()
-    words4=sentences[3].split()
+if num=<5:
     words5=sentences[4].split()
-elif num==6:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
-    words3=sentences[2].split()
-    words4=sentences[3].split()
-    words5=sentences[4].split()
+if num=<6:
     words6=sentences[5].split()
-elif num==7:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
-    words3=sentences[2].split()
-    words4=sentences[3].split()
-    words5=sentences[4].split()
-    words6=sentences[5].split()
+if num=<7:
     words7=sentences[6].split()
-else:
-    words1=sentences[0].split()
-    words2=sentences[1].split()
-    words3=sentences[2].split()
-    words4=sentences[3].split()
-    words5=sentences[4].split()
-    words6=sentences[5].split()
-    words7=sentences[6].split()
+if num=<8:
     words8=sentences[7].split()
-
-problem.replace("&", ".")
+if num=<9:
+    words9=sentences[8].split()
+if num==10:
+    words10=sentences[10].split()
