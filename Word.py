69618a6cc5792eeb7d596990353e99bf268b2a3a
Suhan Gui <suhangui@hanovernorwichschools.org>
2015-11-18 14:00:20 -0500

    Updated from Brython Server: 11/18/2015 2:00:19 PM

--- Word.py
+++ Word.py
@@ -22,10 +22,9 @@ for x in dec_range:
     j=deci[x]
     jo=str(j)
     word_=problem.split()
-    print( ", ".join( repr(e) for e in word__ ) )
-    print(word_)
-    word_.replace(jo)
-    print(word_)
+    suhan=", ".join( repr(e) for e in word_ )
+    suhan.replace(jo)
+    print(suhan)
 
 sentences = re.split(' *[\.\?!][\'"\)\]]* *', problem) #split the sentences!
 
