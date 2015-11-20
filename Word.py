fdc9e9bfee48d9859e6c9dd62d9c31218c0276c6
Suhan Gui <suhangui@hanovernorwichschools.org>
2015-11-20 12:55:48 -0500

    Updated from Brython Server: 11/20/2015 12:55:46 PM

--- Word.py
+++ Word.py
@@ -12,16 +12,16 @@ problem=input("What is your mathematical word problem? (Spelling, grammar, and p
 
 problem_=''.join( c for c in problem if  c not in '$')
 
-deci=re.findall("\d+.\d+", problem_)
+deci=re.findall("\d+.\d+", problem_) #find decimals
 len_dec=len(deci)
 dec_range=list(range(0,len_dec))
 dci=str(deci)
 word_=problem_.split()
-list0 = [item for item in word_ if item not in deci]
+list0 = [item for item in word_ if item not in deci] #DESTROY THE DECIMALS!
 
-inte=[int(s) for s in problem__.split() if s.isdigit()]
+inte=[int(s) for s in problem__.split() if s.isdigit()] #Locate integers
 inte_=str(inte)
-list1 = [item for item in list0 if item not in inte_]
+list1 = [item for item in list0 if item not in inte_] #DESTROY THE INTEGERS!
 
 problem__=" ".join(list1)
 
@@ -44,18 +44,44 @@ if num>=1:
 if num>=2:
     words2=sentences[1].split()
 if num>=3:
-    words1=sentences[0].split()
+    words3=sentences[0].split()
 if num>=4:
-    words2=sentences[1].split()
+    words4=sentences[1].split()
 if num>=5:
-    words1=sentences[0].split()
+    words5=sentences[0].split()
 if num>=6:
-    words2=sentences[1].split()
+    words6=sentences[1].split()
 if num>=7:
-    words1=sentences[0].split()
+    words7=sentences[0].split()
 if num>=8:
-    words2=sentences[1].split()
+    words8=sentences[1].split()
 if num>=9:
-    words1=sentences[0].split()
+    words9=sentences[0].split()
 if num==10:
-    words2=sentences[1].split()
+    words10=sentences[1].split()
+    
+"""
+#Finale!
+print("Ok. Lets split your problem into statements
+
+if num>=1:
+    print("For
+if num>=2:
+    
+if num>=3:
+    
+if num>=4:
+    
+if num>=5:
+    
+if num>=6:
+    
+if num>=7:
+    
+if num>=8:
+    
+if num>=9:
+    
+if num==10:
+    
+"""
