e70e6947d0a65c554bd697c6bc5a47c6f10b58c4
Suhan Gui <suhangui@hanovernorwichschools.org>
2015-12-01 14:37:05 -0500

    Updated from Brython Server: 12/1/2015 2:37:04 PM

--- Word.py
+++ Word.py
@@ -59,7 +59,7 @@ divisions__=['/']
 equal=['is equal to']
 equal__=['=']
 
-los_questions=['When','What','when','what','how much','How much']
+los_questions=['When','when','how much','How much','What time','what time','how long did it take','How long did it take','How long was','how long was']
 
 if num >0 and num <=10:
     print("\nOk. Lets split your problem into statements:")
@@ -72,7 +72,7 @@ if num >0 and num <=10:
         list2 = [item for item in wordd if item not in inte_] #DESTROY THE INTEGERS!
     
         if num_of_ints==0:
-            if len_dec==0
+            if len_dec==0:
                 if donfail==1:
                     satup=[i for i in list2 if i in los_questions]
                     sattup=len(satup)
@@ -86,16 +86,27 @@ if num >0 and num <=10:
                 questionhi=False
         else:
             questionhi=False
-    
+        
         if questionhi==True:
-            print("This sentence is a question")
-            if satup=='when':
-                print("This question is asking when it is")
-            elif satup==:
+            confirmyes=input("Sentence {0} is a question, right? y to confirm. n is reject")
+            confirmyes.lower()
+            if confirmyes=="y":
+                print("This sentence is a question")
+                if satup=='when' or satup=="When":
+                    print("This question is asking when it is")
+                    
+                elif satup=="how much" or satup=="How much":
+                    print("This is asking how much the is")
+                elif satup=="what time" or satup=="What time":
+                    print("This is asking what time it is")
+                elif satup=="How long" or satup=="how long":
+                    print("This is asking 
                 
-            elif satup==2:
-                print("")
-            elif satup==2:
-                print("")
+                else:
+                    print("This is not a question mate")
+                    questionhi=False
+            elif confirmyes=="n":
+                questionhi=False
+
         elif questionhi==False:
             print("")
